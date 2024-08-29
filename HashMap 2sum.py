03cb3e0ccd1e03f9e324f1cc6501a95ae8cefcbf
shigeosapsycho <157628053+shigeosapsycho@users.noreply.github.com>
2024-08-28 21:15:01 -0700

    Update HashMap 2sum.py

--- HashMap 2sum.py
+++ HashMap 2sum.py
@@ -1,9 +1,10 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        seen = {}
-        for i in range(len(nums)):
+        seen = {} # Creates a hash map (otherwise known as an empty dictonary) to store numbers
+        for i in range(len(nums)): # This for loop will take every number in the list 'nums'
             complement = target - nums[i]
             if complement in seen:
                 return [seen[complement], i]
-            seen[nums[i]] = I
+            seen[nums[i]] = i 
 #42 ms
+# No return function is needed since the LeetCode problem is guaranteed one solution for every list.
