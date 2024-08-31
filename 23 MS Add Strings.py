4689b0abba0adfa212ede2375133f273649ba638
shigeosapsycho <157628053+shigeosapsycho@users.noreply.github.com>
2024-08-31 03:06:16 -0700

    Create 23 MS Add Strings.py

---
+++ 23 MS Add Strings.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+        nums1 = int(num1)
+        nums2 = int(num2)
+        x = nums1+nums2
+        return str(x)
+
+# 23 ms run time
