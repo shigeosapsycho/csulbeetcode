2ab39cc355a3c3098725aadecadb4c56825ae5c2
shigeosapsycho <157628053+shigeosapsycho@users.noreply.github.com>
2024-09-02 13:09:21 -0700

    Update 21 ms Power of Two.py

--- 21 ms Power of Two.py
+++ 21 ms Power of Two.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def isPowerOfTwo(self, n):
-        if n <= 0:
+        if n <= 0: # checks if it is a not a power of 2, powers of 2 must be a positive integer
             return False
-        return n & (n - 1) == 0
+        return n & (n - 1) == 0 
     
 # 21 ms
