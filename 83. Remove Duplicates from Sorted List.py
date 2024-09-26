a6a1336fcf7fb08c598ef6a59ad9c5c934fb8f16
Brandon <brandotram@gmail.com>
2024-09-26 01:40:51 -0700

    solved leet code problem 83!

---
+++ 83. Remove Duplicates from Sorted List.py
@@ -0,0 +1,17 @@
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        current = head # initialize the pointer
+        if not head:
+            return None
+        # Traverse the list while there are at least two nodes to compare (current and current.next)
+        while current and current.next: 
+            if current.val == current.next.val:
+                # Skip the next node by making current.next point to current.next.next
+                # This removes the duplicate from the list
+                current.next = current.next.next
+                pass
+            else:
+                current = current.next # If the current node is unique (no duplicate), move to the next node
+        return head # After processing the entire list, return the modified head of the list
+        
+# 33 ms O(n)
