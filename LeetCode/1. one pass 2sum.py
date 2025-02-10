b62200611d982fdbc93d55d1f0c83a5ba7d035a3
brandon <157628053+shigeosapsycho@users.noreply.github.com>
2025-02-10 01:30:33 -0800

    Update 1. one pass 2sum.py
    added comments

--- LeetCode/1. one pass 2sum.py
+++ LeetCode/1. one pass 2sum.py
@@ -1,12 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        prevMap = {}
-        i = 0
+        prevMap = {} # Initalize an empty HashMap
+        i = 0 # Also initalize the counter to 0
 
-        for n in nums:
-            diff = target - n
-            if diff in prevMap:
-                return [prevMap[diff], i]
-            prevMap[n] = i
-            i += 1
+        for n in nums: 
+            diff = target - n # Calculate the difference between the target and the current number n
+            if diff in prevMap: # We are looking if the difference (target - n) is in our hashmap
+                return [prevMap[diff], i] # If the difference exists in the HashMap, return the indices of the two numbers that add up to the target
+            prevMap[n] = i # Otherwise, if it is not in the hash map, we would add key n with value i
+            i += 1 # Increment i by 1 to go through the input
         return []
